36fdb995ec9c0b4200b40f126a442e5f3a9af002
ZenidX <zenid77@gmail.com>
2026-08-05 23:01:20 +0200

    Avanzando

--- packs/c/c05/repo/ex08/ft_ten_queens_puzzle.c
+++ packs/c/c05/repo/ex08/ft_ten_queens_puzzle.c
@@ -3,16 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ten_queens_puzzle.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xalara <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: xalara <zenid77@gmail.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/28 21:18:37 by xalara            #+#    #+#             */
-/*   Updated: 2026/07/29 14:14:28 by xalara           ###   ########.fr       */
+/*   Created: 2026/08/05 11:48:54 by xalara            #+#    #+#             */
+/*   Updated: 2026/08/05 16:14:01 by xalara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_intcpy(int *dest, int *src)
+int	ft_q_ava(int pos[10], int p)
+{
+	int	i;
+
+	i = 0;
+	while (i < 10)
+	{
+		if ((pos[i] == pos[p]
+				|| pos[i] + p - i == pos[p]
+				|| pos[i] - p + i == pos[p])
+			&& i != p)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	ft_putpos(int pos[10])
+{
+	char	d;
+	int		i;
+
+	i = 0;
+	while (i < 10)
+	{
+		d = pos[i] + '0';
+		write(1, &d, 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+void	ft_intcpy(int dest[10], int src[10])
 {
 	int	i;
 
@@ -24,61 +56,28 @@ void	ft_intcpy(int *dest, int *src)
 	}
 }
 
-int	ft_q_available(int *pos, int n, int p)
+int	ft_q_isolver(int poz[10], int p)
 {
+	int	pos[10];
 	int	i;
-
-	i = -n;
-	while (!(i + p > 0))
-		i++;
-	while (i + p < n)
-	{
-		if (i != 0
-			&& (pos[p + i] + i == pos[p]
-				|| pos[p + i] - i == pos[p]
-				|| pos[p + i] == pos[p]))
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-void	ft_putdig(int *pos, int i, int n)
-{
-	char	d;
-
-	while (i < n)
-	{
-		d = pos[i] + '0';
-		write(1, &d, 1);
-		i++;
-	}
-	d = 10;
-	write(1, &d, 1);
-}
-
-int	ft_q_isolver(int *poz, int n, int p)
-{
-	int		pos[10];
-	int		i;
-	int		c;
+	int	r;
 
 	ft_intcpy(pos, poz);
-	c = 0;
+	r = 0;
 	i = 0;
-	while (i < n && p < n)
+	while (i < 10 && p != 10)
 	{
 		pos[p] = i;
-		if (ft_q_available(pos, n, p) == 1)
-			c += ft_q_isolver(pos, n, p + 1);
+		if (ft_q_ava(pos, p))
+			r += ft_q_isolver(pos, p + 1);
 		i++;
 	}
-	if (p == n)
+	if (p == 10)
 	{
-		ft_putdig(pos, i, n);
-		c = 1;
+		ft_putpos(pos);
+		return (1);
 	}
-	return (c);
+	return (r);
 }
 
 int	ft_ten_queens_puzzle(void)
@@ -92,12 +91,5 @@ int	ft_ten_queens_puzzle(void)
 		pos[i] = -100;
 		i++;
 	}
-	return (ft_q_isolver(pos, i, 0));
+	return (ft_q_isolver(pos, 0));
 }
-/*
-int	main(void)
-{
-	ft_ten_queens_puzzle();
-	return (0);
-}
-*/
